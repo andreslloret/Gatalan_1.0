22c33d692396b10d3076c67b4f3763cd2ee9d39a
andres lloret <andreslloretsz@gmail.com>
2023-03-03 11:06:13 +0100

    destroy method

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -18,6 +18,13 @@ class ToursController < ApplicationController
     end
   end
 
+  def destroy
+    @tour = Tour.find(params[:id])
+    @tour.destroy
+    redirect_to tours_path
+  end
+
+
 
   def edit
     @tour = Tour.find(params[:id])
